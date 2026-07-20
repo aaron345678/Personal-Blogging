fc8972b4cdc09d67557a5cec1a3757b53e85c2f4
aaronC34 <94726964+aaron345678@users.noreply.github.com>
2026-07-20 15:14:09 -0700

    added serializer and url.py. First test to localhost unsucessful

---
+++ personal-blog-api/blog/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Article, Tag
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id','name']
+
+class ArticleSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True,read_only=True)
+    tag_ids = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(),source='tags',write_only=True,required=False)
+
+    class Meta:
+        model = Article
+        fields = ['id','title','slug','content','tags','tags_ids','published_at,created_at,updated_at']
+        read_only_fields=['slug','created_at','updated_at']
--- personal-blog-api/blog/views.py
+++ personal-blog-api/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from .models import Article
+from .serializers import ArticleSerializer
 
 # Create your views here.
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all().order_by('-published_at')
+    serializer_class = ArticleSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['tags_name','published_at']
+    order_fields = ['published_at','created_at','title']
